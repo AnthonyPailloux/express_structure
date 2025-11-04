8c218d52911a8513cdc517848d9be7942c980d8d
AnthonyPailloux <anthony.pailloux@laplateforme.io>
2025-11-04 15:02:59 +0100

    coursStructure3

---
+++ src/app.js
@@ -0,0 +1,8 @@
+// import du packet express
+const express = require('express');
+
+// creer l'application express
+const app = express();
+
+// export app
+module.export = app;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,27 @@
-// lancer notre app
-// lancer notre server
-console.log("test");
+// charge les variables d'environement de .env
+require('dotenv').config();
+
+// import du fichier "app.js"
+const app = require('./app');
+
+// recupere le PORT
+const PORT = process.env.PORT;
+
+// verifie que le PORT existe
+if(!PORT){
+    console.log('PORT absent veuillez compléter le fichier .env');
+
+    // stop le programme de lancement de node
+    process.exit(1);
+}
+
+// Démarre le serveur Express et log quand il écoute sur le port choisi
+app.listen(PORT, () => {
+    console.log(`server lancé sur le ${PORT}`);
+    
+});
+
+
+
+
+
