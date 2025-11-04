744b64d83ad5c2238c3bb988a2a23d16673f7c26
AnthonyPailloux <anthony.pailloux@laplateforme.io>
2025-11-04 16:51:50 +0100

    coursStructure5

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 // import du packet express
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 
 // creer l'application express
 const app = express();
 
+app.get('/test', (req, res) => {
+    console.log('Ok coté console');
+    res.send('Ok coté client');
+}) 
+
 // export app
-module.export = app;
+module.exports = app;
--- src/server.js
+++ src/server.js
@@ -10,16 +10,15 @@ const PORT = process.env.PORT;
 // verifie que le PORT existe
 if(!PORT){
     console.log('PORT absent veuillez compléter le fichier .env');
-
     // stop le programme de lancement de node
     process.exit(1);
 }
 
 // Démarre le serveur Express et log quand il écoute sur le port choisi
-app.listen(PORT, () => {
-    console.log(`server lancé sur le ${PORT}`);
+app.listen(PORT, ()=>{
+    console.log('server lancé sur le port 3000');
     
-});
+})
 
 
 
