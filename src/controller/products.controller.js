b755615dd66080e9db9787c69c98f1900721befb
AnthonyPailloux <anthony.pailloux@laplateforme.io>
2025-11-05 13:17:50 +0100

    coursStructure7

---
+++ src/controller/products.controller.js
@@ -0,0 +1,6 @@
+// ici mon controller pour mon crud products
+
+exports.test = (req, res) => {
+    console.log('route test de mon controller product');
+    res.send('route test de mon controller product');
+}
---
+++ src/routes/products.routes.js
@@ -0,0 +1,6 @@
+// Importation du module Router depuis Express pour créer un sous-routeur
+const { Router } = require('express');
+
+// Importation du contrôleur qui gère la logique des routes produits
+const productsController = require('../controllers/products.controller');
+
